e41967b5c906baaacec67520680238cb9efb21b6
Tamado666 <79634969+Tamado666@users.noreply.github.com>
2021-03-06 19:17:45 +0700

    Add files via upload

---
+++ dappaganz/donasi.js
@@ -0,0 +1,17 @@
+const donasi = () => { 
+	return `       
+┏━━━━━━━━━━━━━━━━━━━━
+┃        *_DONASI_*  
+┣━━━━━━━━━━━━━━━━━━━━
+┣━⊱❉ DONASI BOSS ❉⊰━━✿
+┃  
+┣━⊱ *PULSA*
+┣⊱ 0812-7168-4846
+┣━⊱ *Gopay*
+┣⊱ 0812-7168-4846
+┃
+┃Gunain doank, kaga donasi Canda Donasi
+┗━━━━━━━━━━━━━━━━━━━━`
+}
+
+exports.donasi = donasi
